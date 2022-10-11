47a7ddd6322d6d1524a74f568b8b36c3d0d844ee
Mike Devlin <mike@intrepiware.com>
2022-10-10 21:20:39 -0500

    Monte carlo simulation finished; starts with a random hand

--- Cards/Program.cs
+++ Cards/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Cards.Models;
 using Cards.Services.Impl;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using static Cards.Services.Impl.UtilService;
 
 namespace Cards
 {
@@ -9,33 +11,117 @@ namespace Cards
     {
         static void Main(string[] args)
         {
-            var random = new Random();
-            var handService = new CardHandService();
-
-
+            const int iterations = 50000;
             while (true)
             {
-                for (long i = 0; ; i++)
+
+                var hand = Shuffle(null).Take(5).ToList();
+                Console.WriteLine($"Running simulations on {Stringify(hand)}");
+
+                Console.WriteLine("Press a key to continue...");
+                Console.ReadKey();
+
+
+                var simulators = Enumerable.Range(0, 32)
+                                    .Select(x => new HandSimulator(hand, (byte)x, iterations))
+                                    .ToList();
+
+                foreach (var simulator in simulators)
                 {
-                    var cards = Enumerable.Range(0, 51).Select(x => Card.FromInt(x))
-                                                        .OrderBy(x => random.NextDouble())
-                                                        .Take(5)
-                                                        .ToList();
-
-                    var hand = handService.GetHand(cards);
-
-                    if (hand.HandType > HandType.Pair
-                        || (hand.HandType == HandType.Pair && hand.PrimaryRank >= Rank.Jack))
-                    {
-                        Console.WriteLine($"\n\n{cards[0]} {cards[1]} {cards[2]} {cards[3]} {cards[4]}\nFound {hand.HandType}\nIterations: {i + 1}\nPrimary Rank: {hand.PrimaryRank}\nSecondary Rank: {hand.SecondaryRank}");
-                        break;
-                    }
+                    Console.WriteLine($"[{simulator.HeldPositions}]: {Stringify(simulator.HeldCards)}...");
+                    simulator.Process();
                 }
 
-                Console.Write("Go again? [Y/N]: ");
-                if (Console.ReadKey().KeyChar.ToString().ToUpper() != "Y")
-                    break;
+                var mostPoints = simulators.OrderByDescending(x => x.Score).First();
+                Console.WriteLine($"Most Points: {Stringify(mostPoints.HeldCards)} ({mostPoints.Score} pts, {mostPoints.Wins} wins, {mostPoints.Wins * 1.0 / iterations:P2})");
+
+                var mostWins = simulators.OrderByDescending(x => x.Wins).First();
+                Console.WriteLine($"Most Wins: {Stringify(mostWins.HeldCards)} ({mostWins.Score} pts, {mostWins.Wins} wins, {mostWins.Wins * 1.0 / iterations:P2})");
+
+                while (true)
+                {
+                    Console.Write("Run another? [Y, N (0-32)]: ");
+                    var response = Console.ReadLine();
+
+                    if (int.TryParse(response, out var value) && value >= 0 && value < 32)
+                    {
+                        var chosen = simulators.Single(x => x.HeldPositions == value);
+                        Console.WriteLine($"{Stringify(chosen.HeldCards)}:");
+                        Console.WriteLine($"{chosen.Score} points, {chosen.Wins} wins, {chosen.Wins * 1.0 / iterations:P2}");
+                        var handTypes = chosen.HandTypeCount.OrderByDescending(x => x.Key);
+                        foreach (var handType in handTypes)
+                            Console.WriteLine($"{handType.Key}: {handType.Value} wins");
+
+                        Console.WriteLine();
+                    }
+                    else if (response.ToLower() == "y")
+                        break;
+                    else
+                        return;
+                }
+
+                Console.Clear();
             }
+        }
+
+
+    }
+
+    public class HandSimulator
+    {
+        public List<Card> Hand { get; private set; }
+        public List<Card> HeldCards { get; private set; } = new List<Card>();
+        public byte HeldPositions { get; private set; }
+        public int Score { get; private set; }
+        public int Wins { get; private set; }
+        public Dictionary<HandType, int> HandTypeCount { get; private set; } = new Dictionary<HandType, int>();
+        public int Iterations { get; private set; }
+
+        public HandSimulator(List<Card> hand, byte heldPositions, int iterations)
+        {
+            Hand = hand;
+            HeldPositions = heldPositions;
+            Iterations = iterations;
+
+            if ((HeldPositions & 1) == 1)
+                HeldCards.Add(Hand[0]);
+
+            if ((HeldPositions & 2) == 2)
+                HeldCards.Add(Hand[1]);
+
+            if ((HeldPositions & 4) == 4)
+                HeldCards.Add(Hand[2]);
+
+            if ((HeldPositions & 8) == 8)
+                HeldCards.Add(Hand[3]);
+
+            if ((HeldPositions & 16) == 16)
+                HeldCards.Add(Hand[4]);
+        }
+
+        public void Process()
+        { 
+
+            var handService = new CardHandService();
+
+            for(var i = 0; i < Iterations; i++)
+            {
+                var redeal = Shuffle(Hand).Take(5 - HeldCards.Count).ToList();
+                redeal.AddRange(HeldCards);
+
+                var result = handService.GetHand(redeal);
+
+                var score = Score(result, 1);
+                if(score > 0)
+                {
+                    Wins++;
+                    Score += score;
+                }
+
+                HandTypeCount[result.HandType] = HandTypeCount.ContainsKey(result.HandType) ? HandTypeCount[result.HandType] + 1 : 1;
+
+            }
+
         }
     }
 }
---
+++ Cards/Services/Impl/UtilService.cs
@@ -0,0 +1,56 @@
+﻿using Cards.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cards.Services.Impl
+{
+    public static class UtilService
+    {
+        static Random random = new Random();
+
+        public static int Score(Hand hand, int wager)
+        {
+            switch (hand.HandType)
+            {
+                case HandType.StraightFlush:
+                    return hand.PrimaryRank == Rank.Ace ? 250 * wager : 50 * wager;
+                case HandType.FourOfAKind:
+                    return 25 * wager;
+                case HandType.FullHouse:
+                    return 9 * wager;
+                case HandType.Flush:
+                    return 6 * wager;
+                case HandType.Straight:
+                    return 4 * wager;
+                case HandType.ThreeOfAKind:
+                    return 3 * wager;
+                case HandType.TwoPair:
+                    return 2 * wager;
+                case HandType.Pair:
+                    return hand.PrimaryRank >= Rank.Jack ? wager : 0;
+                default:
+                    return 0;
+            }
+        }
+
+        public static string Stringify(List<Card> cards) => cards.Any() ? string.Join(' ', cards) : "{None}";
+
+
+        public static List<Card> Shuffle(List<Card> excludedCards)
+        {
+            var cards = Enumerable.Range(0, 52).Select(x => Card.FromInt(x))
+                                                .OrderBy(x => random.NextDouble())
+                                                .ToList();
+
+            if (excludedCards?.Count > 0)
+            {
+                cards = cards.Where(x => !excludedCards.Any(y => x.Rank == y.Rank && x.Suit == y.Suit)).ToList();
+            }
+
+            return cards;
+
+        }
+    }
+}
